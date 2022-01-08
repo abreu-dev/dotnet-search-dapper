02d1f05da4343709fab8fd32938dacf653205038
gabriel.roberto.abreu@gmail.com <gabriel.roberto.abreu@gmail.com>
2022-01-08 19:06:52 -0300

    Seperando construção de cada Statement

--- src/DotNetSearch.Infra.Data/Repositories/CategoriaRepository.cs
+++ src/DotNetSearch.Infra.Data/Repositories/CategoriaRepository.cs
@@ -26,74 +26,112 @@ namespace DotNetSearch.Infra.Data.Repositories
             _dbConnectionFactory = dbConnectionFactory;
         }
 
+        private string BuildOrderByStatement(SearchSortModel[] searchSortModel, PostgreSqlFilterParser parser)
+        {
+            var orderBy = new StringBuilder();
+
+            foreach (var sort in searchSortModel)
+            {
+                orderBy.Append(
+                    string.Format("{0} {1} {2}",
+                        orderBy.Length > 0 ? "," : "ORDER BY",
+                        parser.BuildPropertyPath(sort.PropertyName),
+                        sort.Direction.GetDescription()
+                    )
+                );
+            }
+
+            return orderBy.ToString();
+        }
+
+        private string BuildPaginationWhereStatement(object lastRow, SearchSortModel[] searchSortModel, PostgreSqlFilterParser parser)
+        {
+            var jsonObject = JObject.Parse(Convert.ToString(lastRow));
+            var jsonProperties = jsonObject.Descendants().Where(property => property is JValue);
+            var lastRowFilter = new Dictionary<string, string>();
+
+            foreach (var sort in searchSortModel)
+            {
+                var value = jsonProperties.FirstOrDefault(property => property.Path == sort.PropertyName);
+                if (value != null)
+                {
+                    var propertyPath = parser.BuildPropertyPath(sort.PropertyName);
+                    var propertyValue = $"'{value}'";
+                    lastRowFilter.Add(propertyPath, propertyValue);
+                }
+            }
+
+            var where = new StringBuilder();
+
+            if (lastRowFilter.Count > 0)
+            {
+                where.Append(
+                    string.Format("AND ({0}) {1} ({2})",
+                        string.Join(",", lastRowFilter.Keys),
+                        searchSortModel.First().Direction == SearchSortDirection.Desc ? "<" : ">",
+                        string.Join(",", lastRowFilter.Values)
+                    )
+                );
+            }
+
+            return where.ToString();
+        }
+
+        private string BuildWhereFilterStatement(string filter, PostgreSqlFilterParser parser)
+        {
+            var filterParsed = parser.Parse(filter);
+            return new StringBuilder()
+                .Append("AND ")
+                .Append(filterParsed)
+                .ToString();
+        }
+
+        private string BuildPaginationLimitStatement(int pageSize)
+        {
+            return string.Format("LIMIT {0}", pageSize);
+        }
+
         public override async Task<IEnumerable<Categoria>> DapperSearch(SearchRequestModel searchRequestModel)
         {
-            var query = new StringBuilder()
+            var baseQuery = new StringBuilder()
                 .AppendLine("SELECT Categoria.* FROM \"Categoria\" Categoria WHERE 1=1");
 
             var parser = new PostgreSqlFilterParser("Categoria");
 
+            if (searchRequestModel.Sort == null || searchRequestModel.Sort.Length <= 0)
+            {
+                searchRequestModel.Sort = new SearchSortModel[]
+                {
+                    new SearchSortModel()
+                    {
+                        PropertyName = nameof(Categoria.Nome).ToLower(),
+                        Direction = SearchSortDirection.Asc
+                    }
+                }.ToArray();
+            }
+
             if (searchRequestModel.LastRow != null)
             {
-                var jsonObject = JObject.Parse(Convert.ToString(searchRequestModel.LastRow));
-                var jsonProperties = jsonObject.Descendants().Where(property => property is JValue);
-                var lastRowFilter = new Dictionary<string, string>();
-
-                foreach (var sort in searchRequestModel.Sort)
-                {
-                    var value = jsonProperties.FirstOrDefault(property => property.Path == sort.PropertyName);
-                    if (value != null)
-                    {
-                        var propertyPath = parser.BuildPropertyPath(sort.PropertyName);
-                        var propertyValue = $"'{value}'";
-                        lastRowFilter.Add(propertyPath, propertyValue);
-                    }
-                }
-
-                if (lastRowFilter.Count > 0)
-                {
-                    var where = new StringBuilder()
-                        .Append("AND ")
-                        .Append(string.Format("({0}) {1} ({2})",
-                        string.Join(",", lastRowFilter.Keys),
-                        searchRequestModel.Sort.First().Direction == SearchSortDirection.Desc ? "<" : ">",
-                        string.Join(",", lastRowFilter.Values)));
-                    query.AppendLine(where.ToString());
-                }
+                var paginationWhereStatement = BuildPaginationWhereStatement(searchRequestModel.LastRow, searchRequestModel.Sort, parser);
+                baseQuery.AppendLine(paginationWhereStatement);
             }
 
             if (!string.IsNullOrEmpty(searchRequestModel.Filter))
             {
-                var where = new StringBuilder().Append("AND ");
-
-                var filterParsed = parser.Parse(searchRequestModel.Filter);
-
-                where.Append(filterParsed);
-                query.AppendLine(where.ToString());
+                var whereFilterStatement = BuildWhereFilterStatement(searchRequestModel.Filter, parser);
+                baseQuery.AppendLine(whereFilterStatement);
             }
 
-            if (searchRequestModel.Sort != null && searchRequestModel.Sort.Length > 0)
-            {
-                var orderBy = new StringBuilder();
-
-                foreach (var sort in searchRequestModel.Sort)
-                {
-                    orderBy.Append(
-                        string.Format("{0} {1} {2}",
-                            orderBy.Length > 0 ? "," : "ORDER BY",
-                            parser.BuildPropertyPath(sort.PropertyName),
-                            sort.Direction.GetDescription()));
-                }
-
-                query.AppendLine(orderBy.ToString());
-            }
+            var orderByStatement = BuildOrderByStatement(searchRequestModel.Sort, parser);
+            baseQuery.AppendLine(orderByStatement);
 
             if (searchRequestModel.PageSize > 0)
             {
-                query.AppendLine(string.Format("LIMIT {0}", searchRequestModel.PageSize));
+                var paginationLimitStatement = BuildPaginationLimitStatement(searchRequestModel.PageSize);
+                baseQuery.AppendLine(paginationLimitStatement);
             }
 
-            var finalQuery = query.ToString();
+            var finalQuery = baseQuery.ToString();
 
             using (var connection = _dbConnectionFactory.CreateConnection())
             {
